502f2a279e5b57b97740cbc45a07fba1d1d6322a
Sam Toohey <hello@samtoohey.xyz>
2024-04-09 22:55:35 +1000

    Initial build and component adjustmetn
    adding in new components for header page

---
+++ components/about.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Intro() {
+  return (
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <Image
+        src="/profile.png"
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      />
+    </section>
+  );
+}
