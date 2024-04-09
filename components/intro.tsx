ed63abf1caebbef9d6c7d689e74b558496d6096f
Sam Toohey <hello@samtoohey.xyz>
2024-04-10 08:53:14 +1000

    Intro header final adjustment

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,13 +1,23 @@
 import { CMS_NAME, CMS_URL } from "../lib/constants";
+import Image from "next/image";
 
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      {/* Title */}
+      <h3 className="text-4md md:text-8xl font-bold tracking-tighter leading-tight text-center md:text-left md:pr-8">
         Sam Toohey
-      </h1>
+      </h3>
+
+      {/* Tagline */}
+      {/* Image of yourself */}
+      <div className="flex justify-center mb-4">
+        <div className="w-24 h-24 rounded-full overflow-hidden">
+          <Image src="/me.jpeg" alt="Your Name" width={150} height={150} />
+        </div>
+      </div>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Headless , Developer{" "}
+        Headless, Developer{" "}
         <a
           href={CMS_URL}
           className="underline hover:text-success duration-200 transition-colors"
