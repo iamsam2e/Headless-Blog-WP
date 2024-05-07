a66bb362d5dbb01458aaa31426b5ba47af25873c
Sam Toohey <hello@samtoohey.xyz>
2024-05-07 15:56:56 +1000

    Footer fix and staging older files?

--- components/footer.tsx
+++ components/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
           {/* Links */}
           <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
             <a
-              href="https://middleshelfstudios.com/"
+              href="https://middleshelfstudios.au/"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Visit Middleshelf Studios
